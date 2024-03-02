917acf938cab267c207c4f41385e4bca7374a1df
Mateusbl <mateusbarbosalopes999@gmail.com>
2024-03-02 16:17:00 -0300

    commit dai 02/03

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Restaurantes(models.Model):
-    nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=15)
-    especialidade = models.CharField(max_length=100)
-    horario = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=15)
+    cuisine = models.CharField(max_length=100)
+    working_hours = models.CharField(max_length=100)
     pass
